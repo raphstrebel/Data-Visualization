78a88d9d783ccb9636a8e525bac30c1b421ab986
raphstrebel <raphael.strebel@epfl.ch>
2018-12-19 13:15:10 +0100

    cleaned comments

--- colorfulTracesFunctions.js
+++ colorfulTracesFunctions.js
@@ -5,107 +5,6 @@
 // http://bl.ocks.org/WilliamQLiu/76ae20060e19bf42d774
 // http://bl.ocks.org/d3noob/a22c42db65eb00d4e369
 
-
-
-/*class MapPlot {
-
-	constructor(svg_element_id) {
-		this.svg = d3.select('#' + svg_element_id);
-
-		// Load the pickup nodes (loaded once even if they appear multiple times)
-		/*const pickup_and_dropoff_nodes_promise = d3.csv("data/cleaned.csv").then((data) => {
-			let pickupNodes = [];
-			var newPickupNode;
-
-			let dropoffNodes = [];
-			var newDropoffNode;
-
-			data.forEach((row) => {
-				newPickupNode = parseFloat(row.pnode);
-				if(!pickupNodes.includes(newPickupNode)) {
-					pickupNodes.push(newPickupNode);
-				}
-
-				newDropoffNode = parseFloat(row.dnode);
-				if(!dropoffNodes.includes(newDropoffNode)) {
-					dropoffNodes.push(newDropoffNode);
-				}
-			});
-
-			return {
-		        Pickup: pickupNodes,
-		        Dropoff: dropoffNodes
-		    };;
-		});
-
-		// Load the dictionary of OSM node ID to (long, lat) tuple
-		const node_id_to_coordinate_promise = $.getJSON("data/OSMToLatLngDictionary.json", function(json) {}).then((data) => {
-			return data;
-		});
-
-		// Load the dictionary of OSM node ID to number of occurences of this node
-		const node_id_to_occurences_promise = $.getJSON("data/nodesToOccurences.json", function(json){}).then((data) => {
-			return data;
-		});*/
-
-
-		/* Make the pickup Icon
-		var pickupIcon = L.icon({
-		    iconUrl: 'images/redIcon.png',
-		    iconSize: [10, 10],
-		    //iconAnchor: [22, 94],
-		    //popupAnchor: [-3, -76],
-		    //shadowUrl: 'my-icon-shadow.png',
-		    //shadowSize: [68, 95],
-		    //shadowAnchor: [22, 94]
-		    //riseOnHover: true,
-		});
-
-		// Make the pickup Icon
-		var dropoffIcon = L.icon({
-		    iconUrl: 'images/blueIcon.png',
-		    iconSize: [10, 10],
-		});
-
-
-		Promise.all([pickup_and_dropoff_nodes_promise, node_id_to_coordinate_promise, node_id_to_occurences_promise]).then((results) => {
-			let pickup_nodes = results[0].Pickup;
-			let dropoff_nodes = results[0].Dropoff;
-			let node_id_to_coordinate = results[1]
-			let node_id_to_occurences = results[2];
-			const all_nodes = Object.keys(node_id_to_occurences);
-
-			// Show all dropoff nodes on the map
-			dropoff_nodes.forEach(function(point) {
-				if(node_id_to_coordinate.hasOwnProperty(point)){
-					L.marker([node_id_to_coordinate[point][0], node_id_to_coordinate[point][1]], {icon: dropoffIcon}).addTo(map);
-				}
-			});
-
-			// Display pickup nodes after 2 seconds
-			setTimeout(showPickupNodes, 2000);
-
-			// Show all pickup nodes on the map
-			function showPickupNodes() {
-				pickup_nodes.forEach(function(point) {
-					if(node_id_to_coordinate.hasOwnProperty(point)){
-						L.marker([node_id_to_coordinate[point][0], node_id_to_coordinate[point][1]], {icon: pickupIcon}).addTo(map).setZIndexOffset(1000);;
-					}
-				});
-			}
-
-
-			/* Too slow to load all nodes
-
-			all_nodes.forEach(function(point) {
-				if(idToLngLat.hasOwnProperty(point)) {
-					L.marker([idToLngLat[point][0], idToLngLat[point][1]]).addTo(map);
-				}
-			});
-		});
-	}
-}*/
-
 // global variable
 const normalNodeRadius = 3;
 const pathNodeRadius = 1.5;
@@ -125,11 +24,6 @@ var scaleY;
 
 var map;
 var marker;
-
-
-
-
-
 
 function whenDocumentLoaded(action) {
 	if (document.readyState === "loading") {
@@ -305,13 +199,6 @@ function handlePickupMouseClick(node) {
 
 	canvas.selectAll(".Dropoff").moveUp();
 	canvas.selectAll(".Pickup").moveUp();
-
-	// must show all nodes of these paths
-	// the paths are in "osmID" so we must use "OSMToLatLngDictionary" to convert them
-
-
-	// EVERY THING BREAKS DOWN TO CONSTRUCTING THIS METHOD : showNode(node), who should draw node on the network (keeping the other ones visible)
-
 }
 
 function showAllDropoffNodes() {
@@ -381,14 +268,6 @@ function handleLegendPickupAndDropoffClick(){
 }
 
 whenDocumentLoaded(() => {
-
-
-	// scale
-	//let sacleMode = d3.scaleLinear()
-
-
-	// projection
-	//let projection = d3.geoMercator()
 
 	// initalize map centered on lausanne region
 	initializeMap();
@@ -553,27 +432,3 @@ whenDocumentLoaded(() => {
 
 
 });
-
-
-
-		/*
-		group.updade()
-			.append("circle")
-			.attr("r" , radius)
-			.attr("transform", function(d) {
-				return "translate("+scaleX(d.plng)+","+ scaleY(d.plat)+")";
-			})
-			.attr("fill", "red");
-*/
-
-
-					//.fill("red")
-
-
-
-	//console.log(max)
-
-
-
-	//map.dragging.disable();
-	// plot object is global, you can inspect it in the dev-console
